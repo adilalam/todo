cfe296ae56b46182c3f1a8b94de4b62369a3f84b
md adil alam <adil.alam@opalina.in>
2020-09-20 09:56:04 +0530

    some chages in user route

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-
 const app = express()
 const port = process.env.PORT || 3000
 
 // app.use((req, res, next) => {
-//     res.status(503).send('This site is under maintains')
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
 // })
 
 app.use(express.json())
@@ -19,17 +26,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-// const User = require('./models/user')
-// const Task = require('./models/task')
-// const main = async () => {
+const Task = require('./models/task')
+const User = require('./models/user')
 
-    // const task = await Task.findById('5f633cec9d54b31708ac0697')
+const main = async () => {
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
     // await task.populate('owner').execPopulate()
     // console.log(task.owner)
 
-    // const user = await User.findById('5f633c0e11a0a33844a62207')
+    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
     // await user.populate('tasks').execPopulate()
     // console.log(user.tasks)
-// }
+}
 
-// main()
+main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -4,7 +4,6 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/tasks', auth, async (req, res) => {
-
     const task = new Task({
         ...req.body,
         owner: req.user._id
@@ -18,11 +17,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        // console.log('hgcghv')
-        // const tasks = await Task.find({})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
@@ -33,7 +53,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findOne({_id, owner: req.user._id})
+        const task = await Task.findOne({ _id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
@@ -45,7 +65,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -55,12 +75,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
 
         if (!task) {
             return res.status(404).send()
         }
 
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
@@ -69,7 +91,7 @@ router.patch('/tasks/:id', async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete({_id: req.params.id, owner: req.user._id})
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             res.status(404).send()
